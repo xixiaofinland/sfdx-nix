be2b26594b98d74fc9d57166b52e6596da94685a
Xi Xiao <xi.xiao007@gmail.com>
2025-11-17 03:26:23 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,9 @@
 {
   description = "Nix flake that provides sfdx.";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
   outputs = {
     self,
     nixpkgs,
@@ -15,7 +13,6 @@
       pkgs = import nixpkgs {
         inherit system;
       };
-
       sfPackage = let
         name = "salesforce-cli";
         version = "2.113.2";
@@ -30,18 +27,19 @@
           yarnLock = "${src}/yarn.lock";
           hash = "sha256-C81c31WVnNoX9AEN2fTZlbfUq46Yp/U6Bk4SmMhkAz4=";
         };
+        # Pin to Node.js 22 to match Salesforce CLI requirements (v2.114.1+ uses Node 22)
+        nodejs = pkgs.nodejs_22;
+        yarn = pkgs.yarn.override { inherit nodejs; };
       in
         pkgs.stdenv.mkDerivation {
           inherit version src;
           pname = name;
-          nativeBuildInputs = with pkgs; [nodejs yarn prefetch-yarn-deps fixup-yarn-lock];
+          nativeBuildInputs = [nodejs yarn pkgs.prefetch-yarn-deps pkgs.fixup-yarn-lock];
           phases = ["unpackPhase" "configurePhase" "buildPhase" "installPhase" "distPhase"];
-
           configurePhase = ''
             export HOME=$PWD/yarn_home
             yarn --offline config set yarn-offline-mirror ${offlineCache}
           '';
-
           buildPhase = ''
             export HOME=$PWD/yarn_home
             export SF_HIDE_RELEASE_NOTES=true
@@ -52,7 +50,6 @@
             patchShebangs --build node_modules
             yarn --offline --production=true run build
           '';
-
           installPhase = ''
             mkdir $out
             mv node_modules $out/
@@ -63,10 +60,9 @@
             cp ./package.json $out
             patchShebangs $out
           '';
-
           distPhase = ''
             mkdir -p $out/tarballs/
-            yarn pack --offline --ignore-scripts --production=true --filename $out/tarballs/sf/.tgz
+            yarn pack --offline --ignore-scripts --production=true --filename $out/tarballs/sf.tgz
           '';
         };
     in {
@@ -83,6 +79,6 @@
       };
       formatter = pkgs.alejandra;
       devShells.default =
-        pkgs.mkShell {buildInputs = with pkgs; [nodejs yarn];};
+        pkgs.mkShell {buildInputs = with pkgs; [nodejs_22 yarn];};
     });
 }
